c2c40ffd98dec689d36590c1efb9902e4c7ac228
leessosso <ssossoking@naver.com>
2022-02-20 21:11:13 +0900

    간단한 점수 올리기 데모

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _idx = 0;
+  int count1 = 0;
+  int count2 = 0;
+  String vicTeam = '';
 
   void _incrementCounter() {
     setState(() {
@@ -43,47 +46,82 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '패스트갬퍼스 강의입니다.',
-            ),
-            Icon(Icons.add),
-            Container(
-              child: Text('난 컨테이너 안이에요'),
-              color: Colors.redAccent,
-            ),
-            TextButton(
-              onPressed: () {
-                print('텍스트버튼 눌림');
-              },
-              child: Text('난 텍스트 버튼!'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('FAB 눌림');
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-          BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기탭'),
-          BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기탭2'),
+      body: Column(
+        children: [
+          Container(height: 50),
+          Text(
+            '탁구 대회',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Container(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text('패스트 대학'),
+              Text('캠퍼스 대학'),
+            ],
+          ),
+          Container(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text('$count1점',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              Text('$count2점',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Container(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          count1 = count1 + 1;
+                          if (count1 > 10) {
+                            vicTeam = '패스트 대학 승리!';
+                          }
+                        });
+                      }),
+                  IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          count1 = count1 - 1;
+                        });
+                      })
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          count2 = count2 + 1;
+                          if (count1 > 10) {
+                            vicTeam = '패스트 대학 승리!';
+                          }
+                        });
+                      }),
+                  IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          count2 = count2 - 1;
+                        });
+                      })
+                ],
+              )
+            ],
+          ),
+          Container(height: 20),
+          Text(vicTeam, style: TextStyle(fontSize: 50, color: Colors.green)),
         ],
-        onTap: (index) {
-          setState(() {
-            _idx = index;
-          });
-          print(index);
-        },
-        currentIndex: _idx,
       ),
     );
   }
